bebdda53c2506046e2bdb92ea9a6de2d406038b7
kbl <m.pietraszek@gmail.com>
2010-10-09 18:11:50 +0200

    sql formating

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,13 +1,26 @@
 class Permission < ActiveRecord::Base
   has_and_belongs_to_many :user_groups
   
-	def all_users
-		User.find_by_sql <<-SQL
-			select users.* 
-			from users, user_groups_users, permissions_user_groups
-			where users.id = user_groups_users.user_id 
-			and user_groups_users.user_group_id = permissions_user_groups.user_group_id
-			and permissions_user_groups.permission_id = #{self.id}
-		SQL
+  def all_users
+    User.find_by_sql <<-SQL
+      select users.* 
+        from users
+            ,user_groups_users
+            ,permissions_user_groups
+       where users.id = user_groups_users.user_id 
+         and user_groups_users.user_group_id = permissions_user_groups.user_group_id
+         and permissions_user_groups.permission_id = #{self.id}
+    SQL
   end
 end
+
+# == Schema Information
+#
+# Table name: permissions
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,16 +1,32 @@
 class UserGroup < ActiveRecord::Base
+  
+  ADMINS = 'Usersadmin'
+
   has_and_belongs_to_many :permissions
   has_and_belongs_to_many :users
 
   validates_presence_of :name
   validates_uniqueness_of :name
   
-	def all_users
-		User.find_by_sql <<-SQL
-			select users.* 
-			from users, user_groups_users
-			where users.id = user_groups_users.user_id 
-			and user_groups_users.user_group_id = #{self.id}
+  def all_users
+    User.find_by_sql <<-SQL
+      select users.* 
+        from users
+            ,user_groups_users
+       where users.id = user_groups_users.user_id 
+         and user_groups_users.user_group_id = #{self.id}
     SQL
-	end
+  end
+
 end
+
+# == Schema Information
+#
+# Table name: user_groups
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
