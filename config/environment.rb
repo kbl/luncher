ff0dd80d3cb4c9832af51d694c2b762b71fdf166
kbl <m.pietraszek@gmail.com>
2010-10-09 16:04:05 +0200

    removal of unused helpers

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -31,7 +31,8 @@ Rails::Initializer.run do |config|
   config.gem 'authlogic'
   config.gem 'chronic'
   config.gem 'lockdown', :lib => false
-  config.gem "factory_girl"
+  config.gem 'factory_girl'
+  config.gem 'haml'
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
