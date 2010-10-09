2d769269ae94ae99c63473787e0ecf13940b5592
kbl <m.pietraszek@gmail.com>
2010-10-09 18:08:58 +0200

    temporal system lockdown removal

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,7 +1,6 @@
 class UserSessionsController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
-  after_filter :set_lockdown_values, :only => :create
   
   def new
     @user_session = UserSession.new
@@ -24,11 +23,4 @@ class UserSessionsController < ApplicationController
     redirect_to new_user_session_url
   end
 
-  private
-
-  def set_lockdown_values
-    if user = @user_session.user
-      add_lockdown_session_values(user)
-    end
-  end
 end
